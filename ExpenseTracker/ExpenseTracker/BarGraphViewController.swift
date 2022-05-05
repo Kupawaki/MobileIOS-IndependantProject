f92a72084329b7438717cebce10d2367c98c36bc
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2022-05-04 20:14:20 -0500

    App is done, but has bugs

--- ExpenseTracker/ExpenseTracker/BarGraphViewController.swift
+++ ExpenseTracker/ExpenseTracker/BarGraphViewController.swift
@@ -6,24 +6,84 @@
 //
 
 import UIKit
+import Firebase
+import Charts
 
-class BarGraphViewController: UIViewController {
-
-    override func viewDidLoad() {
+class BarGraphViewController: UIViewController, ChartViewDelegate
+{
+    @IBOutlet weak var refreshBTN: UIButton!
+    let ref = Database.database().reference()
+    var expense = NSDictionary()
+    var income = NSDictionary()
+    var expenseValues:[String] = []
+    var incomeValues:[String] = []
+    var totalIncome:Double = 0.0
+    var totalExpense:Double = 0.0
+    
+    var barChart = BarChartView()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        fetchData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews()
+    {
+        super.viewDidLayoutSubviews()
+        
+        barChart.delegate = self
+        barChart.frame =
+            CGRect(x: 0, y: 0,
+                   width: self.view.frame.size.width,
+                   height: self.view.frame.size.height - 200)
+        barChart.center = view.center
+        view.addSubview(barChart)
+        
+        let set = BarChartDataSet(entries:[BarChartDataEntry(x: 1.0, y: 1.0)])
+        set.colors = ChartColorTemplates.joyful()
+        let data = BarChartData(dataSet: set)
+        barChart.data = data
     }
-    */
 
+    func fetchData()
+    {
+        ref.child("income").getData(completion:
+        { error, snapshot in guard error == nil else {return;}
+            let income = snapshot.value as! NSDictionary
+            self.incomeValues = income.allValues as! [String]
+            
+            for val:String in self.incomeValues
+            {
+                self.totalIncome += (val as NSString).doubleValue
+            }
+        });
+        
+        ref.child("expense").getData(completion:
+        { error, snapshot in guard error == nil else{return;}
+            let expense = snapshot.value as! NSDictionary
+            self.expenseValues = expense.allValues as! [String]
+            
+            for val:String in self.expenseValues
+            {
+                self.totalExpense += (val as NSString).doubleValue
+            }
+        });
+    }
+    
+    @IBAction func refreshBTNOnClick(_ sender: Any)
+    {
+        var newSet = BarChartDataSet(
+        entries: [
+            BarChartDataEntry(x: 1, y: totalIncome),
+            BarChartDataEntry(x: 2, y: totalExpense)
+        ])
+        newSet.label = "Income / Expense"
+        
+        newSet.colors = ChartColorTemplates.joyful()
+    
+        var newData = BarChartData(dataSet: newSet)
+        barChart.data = newData
+        
+    }
 }
--- ExpenseTracker/ExpenseTracker/PieChartViewController.swift
+++ ExpenseTracker/ExpenseTracker/PieChartViewController.swift
@@ -6,24 +6,84 @@
 //
 
 import UIKit
+import Firebase
+import Charts
 
-class PieChartViewController: UIViewController {
-
-    override func viewDidLoad() {
+class PieChartViewController: UIViewController, ChartViewDelegate
+{
+    @IBOutlet weak var refreshBTN: UIButton!
+    let ref = Database.database().reference()
+    var expense = NSDictionary()
+    var income = NSDictionary()
+    var expenseValues:[String] = []
+    var incomeValues:[String] = []
+    var totalIncome:Double = 0.0
+    var totalExpense:Double = 0.0
+    
+    var pieChart = PieChartView()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        fetchData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews()
+    {
+        super.viewDidLayoutSubviews()
+        
+        pieChart.delegate = self
+        pieChart.frame =
+            CGRect(x: 0, y: 0,
+                   width: self.view.frame.size.width,
+                   height: self.view.frame.size.height - 200)
+        pieChart.center = view.center
+        view.addSubview(pieChart)
+        
+        let set = PieChartDataSet(entries:[PieChartDataEntry(value: 1.0, label: "Default Data")])
+        set.colors = ChartColorTemplates.joyful()
+        let data = PieChartData(dataSet: set)
+        pieChart.data = data
     }
-    */
-
+    
+    func fetchData()
+    {
+        ref.child("income").getData(completion:
+        { error, snapshot in guard error == nil else {return;}
+            let income = snapshot.value as! NSDictionary
+            self.incomeValues = income.allValues as! [String]
+            
+            for val:String in self.incomeValues
+            {
+                self.totalIncome += (val as NSString).doubleValue
+            }
+        });
+        
+        ref.child("expense").getData(completion:
+        { error, snapshot in guard error == nil else{return;}
+            let expense = snapshot.value as! NSDictionary
+            self.expenseValues = expense.allValues as! [String]
+            
+            for val:String in self.expenseValues
+            {
+                self.totalExpense += (val as NSString).doubleValue
+            }
+        });
+    }
+    
+    @IBAction func refreshBTNOnClick(_ sender: Any)
+    {
+        var newSet = PieChartDataSet(
+            entries: [
+                PieChartDataEntry(value: totalIncome, label: "Income"),
+                PieChartDataEntry(value: totalExpense, label: "Expense")
+            ])
+        newSet.colors = ChartColorTemplates.joyful()
+        
+        var newData = PieChartData(dataSet: newSet)
+        pieChart.data = newData
+    }
 }
+
+
+
